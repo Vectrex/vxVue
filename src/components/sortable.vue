d149795fbdc21caf197550562f374e2f98e0b1db
Gregor <info@gregorkofler.com>
2021-11-24 00:18:19 +0100

    Added more slot properties for table headers; removed width property from columns since width can be part of cssClass property

--- src/components/sortable.vue
+++ src/components/sortable.vue
@@ -8,12 +8,11 @@
                 v-for="column in columns"
                 :class="[
                     { 'cursor-pointer' : column.sortable },
-                    column.cssClass,
-                    column.width,
+                    column.cssClass
                 ]"
                 @click="column.sortable ? clickSort(column) : null"
             >
-                <slot :name="column.prop + '-header'" :column="column">
+                <slot :name="column.prop + '-header'" :column="column" :sort-dir="sortDir" :sort-column="sortColumn">
                   <div class="flex items-center"><span>{{ column.label }}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 flex-shrink-0 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" v-if="column.sortable">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="sortDir === 'asc' ? 'M19 9l-7 7-7-7' : 'M5 15l7-7 7 7'" v-if="sortColumn === column" />
