54be7f8e525f33db8786873606340a0117c29ee1
gregor <gregor@mail.agindo.intern>
2024-01-09 12:05:08 +0100

    Disabled switch exposes a slightly different styling

--- src/components/form-switch.vue
+++ src/components/form-switch.vue
@@ -4,30 +4,41 @@
   const props = defineProps(['modelValue'])
   const emit = defineEmits(['update:modelValue'])
 
-  const outerClass = computed (() => props.modelValue ? "bg-vxvue" : "bg-slate-200")
-  const innerClass = computed (() => props.modelValue ? "translate-x-5" : "translate-x-0")
   const inputAttrs = computed(() => {
     let attrs = Object.assign({}, useAttrs())
     delete attrs['class']
     return attrs
   })
+  const disabled = computed( () => useAttrs().disabled || useAttrs().disabled === '')
 </script>
 
 <template>
-  <label :class="$attrs['class']">
+  <label :class="$attrs['class']" tabindex="0">
     <span
-        :class="outerClass"
-        class="inline-flex relative flex-shrink-0 w-11 h-6 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out cursor-pointer focus:ring-2 focus:ring-offset-2 focus:outline-none focus:ring-vxvue"
         role="switch"
+        :class="[
+          'inline-flex relative flex-shrink-0 w-11 h-6 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out cursor-pointer focus:ring-2 focus:ring-offset-2 focus:outline-none focus:ring-vxvue',
+          disabled ? 'bg-slate-200' : (modelValue ? 'bg-vxvue' : 'bg-slate-300')
+        ]"
         :aria-checked="!!modelValue"
         :aria-label="$attrs['aria-label']"
     >
       <span
           aria-hidden="true"
-          :class="innerClass"
-          class="inline-block size-5 bg-white rounded-full ring-0 shadow transition duration-200 ease-in-out transform pointer-events-none"
+          :class="[
+            'inline-block size-5 rounded-full ring-0 shadow transition duration-200 ease-in-out transform pointer-events-none',
+            modelValue ? 'translate-x-5' : 'translate-x-0',
+            disabled ? 'bg-slate-100' : 'bg-white'
+          ]"
       ></span>
-      <input class="hidden" value="1" type="checkbox" @change="emit('update:modelValue', $event.target.checked)" v-bind="inputAttrs" :checked="modelValue" />
+      <input
+          v-bind="inputAttrs"
+          :checked="modelValue"
+          class="hidden"
+          value="1"
+          type="checkbox"
+          @change="emit('update:modelValue', $event.target.checked)"
+      />
     </span>
   </label>
   <slot></slot>
