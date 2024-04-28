8e6825d98d3fa9634d71417905da190e3bdf45b8
Gregor Kofler <info@gregorkofler.com>
2024-04-29 00:02:14 +0200

    Doc: Documentation for pagination component added

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,6 +18,7 @@ export default defineConfig({
           { text: 'FormSelect', link: '/form-select' },
           { text: 'FormSwitch', link: '/form-switch' },
           { text: 'MessageToast', link: '/message-toast' },
+          { text: 'Pagination', link: '/pagination' },
           { text: 'PasswordInput', link: '/password-input' },
           { text: 'Slider', link: '/slider' },
           { text: 'SubmitButton', link: '/submit-button' },
