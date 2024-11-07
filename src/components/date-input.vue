62a88a89135d6250d91e153cb94eea7cd9ec7b79
gregor <info@gregorkofler.com>
2024-11-07 12:11:24 +0100

    Fix: Disabling a date input component should also disable the toggle for the picker and the button for emptying the element

--- src/components/date-input.vue
+++ src/components/date-input.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { CalendarIcon, XMarkIcon } from "@heroicons/vue/24/solid"
   import { parseDate } from "../composables/parseDate"
   import { formatDate } from "@vueuse/core"
   import { computed, ref, useAttrs, watch } from "vue"
@@ -22,8 +23,12 @@
     <div v-if="dateString">
       <div class="block flex items-center w-full form-input bg-vxvue-50" :class="{ 'pr-10': showToggle }">
         <span class="text-vxvue-700">{{ dateString }}</span>
-        <button @click="emit('update:modelValue', null)" class="inline-flex flex-shrink-0 justify-center items-center ml-2 rounded-full focus:text-white focus:outline-none size-4 text-vxvue hover:bg-vxvue-100 hover:text-vue-700 focus:bg-vxvue-700">
-          <svg class="size-2" stroke="currentColor" fill="none" viewBox="0 0 8 8"><path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" /></svg>
+        <button
+          v-if="!inputAttrs.disabled"
+          @click="emit('update:modelValue', null)"
+          class="inline-flex flex-shrink-0 justify-center items-center ml-2 rounded-full focus:text-white focus:outline-none size-4 text-vxvue hover:bg-vxvue-100 hover:text-vue-700 focus:bg-vxvue-700"
+        >
+          <x-mark-icon class="size-4" />
         </button>
       </div>
     </div>
@@ -38,14 +43,13 @@
     >
     <button
       v-if="showToggle"
-      class="flex absolute inset-y-0 right-0 items-center px-2 text-vxvue-700 hover:text-vxvue"
-      aria-label="datepicker-toggle"
+      :class="['flex absolute inset-y-0 right-0 items-center px-2 text-vxvue-700', inputAttrs.disabled ? 'cursor-not-allowed' : 'hover:text-vxvue']"
+      :disabled="inputAttrs.disabled"
       @click.stop="emit('toggle-datepicker')"
       ref="toggleButton"
+      aria-label="datepicker-toggle"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" class="size-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-      </svg>
+      <calendar-icon class="size-6" />
     </button>
     <slot />
   </div>
