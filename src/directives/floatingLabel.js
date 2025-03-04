0233a865347d55bc6478e517e9cbee6129d97003
gregor <info@gregorkofler.com>
2025-03-04 17:01:14 +0100

    Feat: Floating label directive tries to find input or textarea element if applied to a wrapping component/element

--- src/directives/floatingLabel.js
+++ src/directives/floatingLabel.js
@@ -1,28 +1,32 @@
 const floatingLabel = {
     mounted: (el, binding) => {
-        const id = Math.random().toString(36).substring(2)
-        const div = document.createElement('div')
-        div.classList.add('relative')
-        const label = document.createElement('label')
-        label.classList.add(
-            'absolute', 'px-2', 'left-1', 'top-2', 'scale-90', 'text-sm', 'z-10', 'origin-[0]', 'bg-white', '-translate-y-4', 'transform-gpu', 'duration-200',
-            'peer-focus:text-blue-600', 'peer-focus:top-2', 'peer-focus:scale-90', 'peer-focus:-translate-y-full',
-            'peer-placeholder-shown:scale-100', 'peer-placeholder-shown:-translate-y-1/2',
-            el.nodeName === 'TEXTAREA' ? 'peer-placeholder-shown:top-4' : 'peer-placeholder-shown:top-1/2'
-        )
-        if (binding.value?.invalid) label.classList.add('text-error')
-        if (el.getAttribute('required') !== null) label.classList.add('required')
+        while (el && !['INPUT', 'TEXTAREA'].includes(el.nodeName)) el = el.firstChild
 
-        label.setAttribute('for', el.getAttribute('id') || (id + '-element'))
-        label.innerHTML = binding.value?.label || el.getAttribute('placeholder') || (id + '-label')
+        if (el) {
+            const id = Math.random().toString(36).substring(2)
+            const div = document.createElement('div')
+            div.classList.add('relative')
+            const label = document.createElement('label')
+            label.classList.add(
+                'absolute', 'px-2', 'left-1', 'top-2', 'scale-90', 'text-sm', 'z-10', 'origin-[0]', 'bg-white', '-translate-y-4', 'transform-gpu', 'duration-200',
+                'peer-focus:text-blue-600', 'peer-focus:top-2', 'peer-focus:scale-90', 'peer-focus:-translate-y-full',
+                'peer-placeholder-shown:scale-100', 'peer-placeholder-shown:-translate-y-1/2',
+                el.nodeName === 'TEXTAREA' ? 'peer-placeholder-shown:top-4' : 'peer-placeholder-shown:top-1/2'
+            )
+            if (binding.value?.invalid) label.classList.add('text-error')
+            if (el.getAttribute('required') !== null) label.classList.add('required')
 
-        el.classList.add('peer')
-        el.setAttribute('placeholder', ' ')
-        el.setAttribute('id', el.getAttribute('id') || (id + '-element'))
+            label.setAttribute('for', el.getAttribute('id') || (id + '-element'))
+            label.innerHTML = binding.value?.label || el.getAttribute('placeholder') || (id + '-label')
 
-        el.parentElement.replaceChild(div, el)
-        div.appendChild(el)
-        div.appendChild(label)
+            el.classList.add('peer')
+            el.setAttribute('placeholder', ' ')
+            el.setAttribute('id', el.getAttribute('id') || (id + '-element'))
+
+            el.parentElement.replaceChild(div, el)
+            div.appendChild(el)
+            div.appendChild(label)
+        }
     }
 }
 export default floatingLabel
