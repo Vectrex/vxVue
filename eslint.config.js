2401dcbe5b8f1d4bb382487779758f3df94f1ea1
Gregor Kofler <info@gregorkofler.com>
2025-05-05 16:25:08 +0200

    Chore: Dependencies updated

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import pluginVue from 'eslint-plugin-vue'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   ...pluginVue.configs['flat/recommended'],
+  ...stylistic.configs.recommended,
   {
     ignores: ['docs/'],
     rules: {
