054653ee73bfa4394b157c81eade8a972243fbb7
gregor <info@gregorkofler.com>
2025-10-28 11:38:17 +0100

    Test: Simple test for FormSwitch component added

--- src/components/__tests__/FormSwitch.test.js
+++ src/components/__tests__/FormSwitch.test.js
@@ -6,11 +6,14 @@ describe("FormSwitch", () => {
     test("emits update:modelValue when clicked", async () => {
         const wrapper = mount(FormSwitch, {
             props: {
-                modelValue: false,
-                'onUpdate:modelValue': e => wrapper.setProps({ modelValue: e })
-            }});
-        await wrapper.find("input").trigger("change");
+                modelValue: false
+            },
+            // required so that the click event registers
+            attachTo: document.body,
+        });
+        const input = wrapper.find("input");
+        await input.trigger("click");
         expect (wrapper.emitted()).toHaveProperty("update:modelValue");
-        // expect(wrapper.emitted()["update:modelValue"][0]).toEqual([true]);
+        expect (wrapper.emitted()["update:modelValue"]).toBeTruthy();
     })
 })
